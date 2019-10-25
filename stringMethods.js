da87104d1610c22dc122890179dc3f3d5ea038fc
Danilo Sampaio <daniloarpesam@gmail.com>
2019-10-25 12:10:07 -0300

    Create stringMethods.js

---
+++ stringMethods.js
@@ -0,0 +1,93 @@
+/* 20 String Methods */
+
+var stringOne = "freeCodeCamp is the best place to learn"
+var stringTwo = "frontend and backend development"
+
+// charAt()
+console.log(stringOne.charAt(1))
+
+// charCodeAt()
+console.log(stringOne.charCodeAt(1))
+
+// concat()
+console.log(stringOne.concat(stringTwo))
+
+// endsWith()
+console.log(stringOne.endsWith("to"))
+
+// fromCharCode()
+console.log(String.fromCharCode(114))
+
+
+// includes()
+console.log(stringTwo.includes("end"))
+
+
+// indexOf()
+console.log(stringTwo.indexOf("end"))
+
+// lastIndexOf()
+console.log(stringTwo.lastIndexOf("end"))
+
+// match()
+console.log(stringTwo.match(/end/g))
+
+// repeat()
+console.log(stringOne.repeat(3))
+
+
+// replace()
+console.log(stringTwo.replace(/end/g, "END"))
+
+// search()
+console.log(stringTwo.search("end"))
+
+// slice()
+console.log(stringTwo.slice(2, 4))
+
+// split()
+console.log(stringOne.split(" "))
+
+// startsWith()
+console.log(stringOne.startsWith("free"))
+
+// substr()
+console.log(stringTwo.substr(2, 4))
+
+// substring()
+console.log(stringTwo.substring(2, 4))
+
+// toLowerCase()
+console.log(stringOne.toLowerCase())
+
+// toUpperCase()
+console.log(stringOne.toUpperCase())
+
+// trim()
+var stringThree = "     Subscribe now!      ";
+console.log(stringThree.trim())
+
+
+
+// RESULTS 
+
+"r"
+114
+"freeCodeCamp is the best place to learnfrontend and backend development"
+false
+"r"
+true
+5
+17
+["end", "end"]
+"freeCodeCamp is the best place to learnfreeCodeCamp is the best place to learnfreeCodeCamp is the best place to learn"
+"frontEND and backEND development"
+5
+"on"
+["freeCodeCamp", "is", "the", "best", "place", "to", "learn"]
+true
+"onte"
+"on"
+"freecodecamp is the best place to learn"
+"FREECODECAMP IS THE BEST PLACE TO LEARN"
+"Subscribe now!"
