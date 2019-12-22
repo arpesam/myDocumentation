ba111eeebb179087471e745ee1305b26d1c3a06d
Danilo <daniloarpesam@gmail.com>
2019-12-22 15:25:35 -0300

    adds array descriptions

--- array-methods.js
+++ array-methods.js
@@ -29,7 +29,7 @@ console.log(evens);
 const sum = [1, 2, 3].reduce(function (result, item) {
   return result + item;
 }, 0);
-console.log(sum)
+console.log('reduce -> ', sum)
 
 
 // some
@@ -67,34 +67,32 @@ console.log(foundIndex)
 
 var arr = ["a", "b", "c"];
 
-arr.push("d");
+arr.push("d"); // add value in array
 console.log(arr);
 
-console.log(arr.pop());
+console.log(arr.pop()); // removes the last value
 console.log(arr);
 
 var arr2 = ["g", "q"];
-console.log(arr.concat(arr2));
+console.log(arr.concat(arr2)); // sum two arrays
 console.log(arr2);
 
-console.log(arr.join("!"));
+console.log(arr.join("!")); // put the values together with ! -> 'a!b!c'
 
-console.log(arr.reverse());
+console.log(arr.reverse()); // revert the values 
 console.log(arr);
 
-console.log(arr.shift());
+console.log(arr.shift()); // removes the first value
 console.log(arr);
 
-console.log(arr.unshift("p"));
+console.log(arr.unshift("p")); // put a value at the first position
 console.log(arr);
 
-console.log(arr.slice(1,3));
+console.log(arr.slice(1,3)); // cut the 1 till 3 position 
 
-arr.push("i");
-arr.push("f");
-arr.sort(arr);
+console.log(arr.sort(arr)); // reorginize the array - The sort order can be either alphabetic or numeric
 console.log(arr);
 
-console.log(arr.splice(2, 2, "JS Nuggets"));
+console.log(arr.splice(2, 2, "JS Nuggets")); // removes the second position and add 'JS Nuggets'
 console.log(arr);
 
